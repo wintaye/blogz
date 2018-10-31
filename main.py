aa2463aa771baffa2829685e8a07bda12b246d27
wintaye <wintaye@gmail.com>
2018-10-30 22:47:25 -0500

    a little more functional, still some errors

--- main.py
+++ main.py
@@ -56,8 +56,8 @@ class Blog(db.Model):
 
 # @app.before_request
 # def require_login():
-#     allowed_routes = ['login', 'signup']
-#     if request.endpoint not in allowed_routes and 'email' not in session:
+#     allowed_routes = ['login', 'signup', 'blog', 'index']
+#     if request.endpoint not in allowed_routes and 'username' not in session:
 #         return redirect('/login')
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -66,56 +66,48 @@ def login():
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
-        if not user:
-            flash("Username not found :/")
-            return redirect('/signup')
-        elif user.password != password:
-            flash("Wrong Password")
-            return redirect("/login")
-        else:
+        if user and user.password == password:
             session['username'] = username
             flash("Logged In!")
             return redirect('/blog')
+        else:
+            flash("Username not found or password incorrect :/")
+            return render_template('login.html')
     else:
         return render_template('login.html')
-    
 
-# @app.route("/blog", methods=['GET', 'POST'])
-# def display_blog_posts():
-@app.route('/blog')
-@app.route('/blog/<bid>', methods=['GET'])
+@app.route('/blog', methods=['GET', 'POST'])
 def display_blog_post():
-    blogs = Blog.query.all()
-    blogid=request.args.get('bid')
-    if blogid:
-        new_post = Blog.query.get(blogid)
-    #display all posts or new blog post page
-        blog = Blog.query.get(blog_id)
-        blog = Blog.query.order_by(blog_id.desc()).all()
-        return render_template('single_post.html', title="New Blog Post", blog=blog)
-    else:  
+    # if request.method == 'POST':
+    #     blogs = Blog.query.all()
+    #     # user_id = request.args.get('user')
+    #     # new_post = Blog.query.get(blogid)
+    #     return render_template('single_post.html', title="New Blog Post", blogs=blogs)
+    # else:  
         all_blog_posts = Blog.query.all()
         return render_template('all_blog_posts.html', title="All Blog Entries", all_blog_posts=all_blog_posts)
 
 @app.route("/new_post", methods=['GET', 'POST'])
 def new_entry():
     #add new blog post to db
+    #if request is POST
     if request.method == 'POST':
         new_blog_title = request.form['title']
         new_blog_body = request.form['body']
-        owner_id = request.args.get('owner_id')
+        #owner_id = request.args.get('owner_id')
         #username = session['username']
         #new_blog_owner = User.query.filter_by(username=username).first()
-        new_blog_post = Blog(new_blog_title, new_blog_body, owner_id)
         if not new_blog_title or not new_blog_body:
             flash("Please fill out all forms.")
             return render_template('new_post_form.html', title="Create a new blog post", new_blog_title=new_blog_title, new_blog_body=new_blog_body)
         else:
+            new_blog_post = Blog(new_blog_title, new_blog_body, owner_id)
             db.session.add(new_blog_post)
             db.session.commit()
             url = "/blog?id=" + str(new_blog_post.id)
             return redirect(url)
     else:
+        #if request is GET
         return render_template('new_post_form.html')
 
 @app.route("/signup", methods=['POST', 'GET'])
@@ -128,7 +120,6 @@ def signup():
         password_error = ''
         email_error = ''
         blank_error = ''
-        password_error_2 = ''
         verify_password_error = ''
         email_error = ''
         existing_user = User.query.filter_by(username=username).first()
@@ -137,11 +128,12 @@ def signup():
             flash("Username is taken.")
             return redirect('/signup')
 
-        if not user_is_valid(username, email, password, verify_password):
+        elif not user_is_valid(username, email, password, verify_password):
             return render_template('signup.html', email_error=email_error, verify_password_error=verify_password_error, email=email, username=username, blank_error=blank_error, password_error=password_error)
             
         else:
-            new_user = User(username, email, password)
+            session['username'] = username
+            new_user = User(email, password, username)
             db.session.add(new_user)
             db.session.commit()
             return redirect("/blog")
@@ -149,8 +141,8 @@ def signup():
     else: 
         return render_template('signup.html')
 
-@app.route('/singleUser')
-@app.route('/singleUser/<user>')
+@app.route("/singleUser")
+@app.route("/singleUser/<user>")
 def singleUser():
     user_name_fetch = request.args.get('user')
     user = User.query.filter_by(username=user_name_fetch).first()
@@ -158,6 +150,15 @@ def singleUser():
     blogs = Blog.query.filter_by(owner_id=owner_id)
     return render_template('single_user.html', user=user, blogs=blogs)
 
+@app.route("/single_post")
+def singlepost():
+    blogid = request.args.get('bid')
+    if blogid:
+        blog = Blog.query.get(blogid)
+        return render_template('single_post.html', blog=blog)
+    else: 
+        all_blog_posts = Blog.query.all()
+        return render_template('all_blog_posts.html', title="All Blog Entries", all_blog_posts=all_blog_posts)
 
 @app.route("/")
 def all_users():
